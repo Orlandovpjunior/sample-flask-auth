5ad67e7d3c8d7a5c480a88dbfc3fbf7238d1196f
orlandovpjunior <orlando.virginio.penha.junior@ccc.ufcg.edu.br>
2025-04-02 18:00:41 -0300

    chore: encriptar senhar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import bcrypt
 from database import db
 from models.user import User
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
@@ -28,7 +29,7 @@ def login():
     if username and password:
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             return jsonify({'message': 'Autenticação realizada com sucesso'})
 
@@ -47,7 +48,8 @@ def create_user():
     password = data.get('password')
 
     if username and password:
-        user = User(username=username, password=password, role='user')
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({'message': 'Usuário cadastrado com sucesso'})
